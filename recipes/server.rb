b8d857d3c7d5e6fc37ba5a45b275221828751865
PK035158 <praveereddy.katta@cerner.com>
2016-01-28 21:31:18 +0000

    Added tests, added dynamic values, updated version

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,7 +5,9 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 package 'apache2'
 file '/var/www/html/index.html'do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1><b/>
+          <p>Ipaddress: #{node['ipaddress']}</p><b/>
+          <p>Hostname: #{node['hostname']}</p><b/>"
   action :create
 end
 file '/etc/init/httpd.conf' do
