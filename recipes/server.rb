09ee1e01ebbeb5bb29b3cfc32961b2910775511b
PK035158 <praveereddy.katta@cerner.com>
2016-01-28 22:17:05 +0000

    Added templates

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,11 +4,12 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 package 'apache2'
-file '/var/www/html/index.html'do
-  content "<h1>Hello, world!</h1><b/>
-          <p>Ipaddress: #{node['ipaddress']}</p><b/>
-          <p>Hostname: #{node['hostname']}</p><b/>"
-  action :create
+template '/var/www/html/index.html'do
+  #content "<h1>Hello, world!</h1><b/>
+  #        <p>Ipaddress: #{node['ipaddress']}</p><b/>
+  #        <p>Hostname: #{node['hostname']}</p><b/>"
+  #action :create
+  source 'index.html.erb'
 end
 file '/etc/init/httpd.conf' do
 end
